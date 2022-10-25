e04a0f42aa1d87c601a239cac592944e09a9dcff
wonbong jung <kindfamily@kako.com>
2022-10-26 06:23:36 +0900

    test

--- KoreaStockAutoTrade.py
+++ KoreaStockAutoTrade.py
@@ -15,7 +15,7 @@ DISCORD_WEBHOOK_URL = _cfg['DISCORD_WEBHOOK_URL']
 URL_BASE = _cfg['URL_BASE']
 
 def send_message(msg):
-    """디스코드 메세지 전송test"""
+    """디스코드 메세지 전송"""
     now = datetime.datetime.now()
     message = {"content": f"[{now.strftime('%Y-%m-%d %H:%M:%S')}] {str(msg)}"}
     requests.post(DISCORD_WEBHOOK_URL, data=message)
